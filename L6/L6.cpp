3531e151acc9bc25256caf9f05b76dd9fea1700d
EvgenyT <60173532+EvgenyTen@users.noreply.github.com>
2020-04-28 19:27:40 +0300

    Add project files.

---
+++ L6/L6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+ cout << "Master started!"<<endl;
+
+
+
+
+ system("pause");
+ return 0;
+}
+
